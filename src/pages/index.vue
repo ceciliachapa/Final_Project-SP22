041a2ca406adb5409fb5bf1892bce6cad28ce213
ceciliachapa <cecilia.chapa91@gmail.com>
2022-03-06 12:28:09 -0600

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<script setup>
+import userAuth from "../composable/useAuth";
+const { isAuthenticated, user } = userAuth();
+</script>
+
+<template>
+  <div
+    class="
+      flex flex-col
+      items-center
+      justify-center
+      space-y-16
+      min-h-screen-nonav
+    "
+  >
+    <h2
+      v-if="!isAuthenticated"
+      class="
+        text-6xl
+        tracking-tighter
+        font-bold
+        text-yellow-700
+        font-mono
+        text-center
+      "
+    >
+      <h2>Welcome</h2>
+      <br />
+      <img src="../assets/welcome.png" alt="Hello" />
+    </h2>
+    <h2
+      v-else
+      class="
+        text-6xl
+        tracking-tighter
+        font-bold
+        text-yellow-700
+        font-mono
+        text-center
+      "
+    >
+      <h2>
+        <p>Welcome {{ user }}...</p>
+      </h2>
+      <br />
+      <img src="../assets/welcome.png" alt="Hello" />
+    </h2>
+  </div>
+</template> 
